b4620a61f8995f825226de9f0e09236f792ca88e
Alexandre Felix <felix.psn.bra@hotmail.com>
2019-12-26 14:40:52 -0300

    Added virtual fields on appointment model

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import { isBefore, subHours } from 'date-fns';
 
 class Appointment extends Model {
   static init(sequelize) {
@@ -6,6 +7,22 @@ class Appointment extends Model {
       {
         date: Sequelize.DATE,
         canceled_at: Sequelize.DATE,
+
+        // Campo virtual para demonstrar se a data e horário agendados já passou
+        past: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(this.date, new Date());
+          },
+        },
+        // Campo virtual para demonstrar se o agendamento é cancelável. Possuindo
+        // um prazo de mais de duas horas do agendamento.
+        cancelable: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(new Date(), subHours(this.date, 2));
+          },
+        },
       },
       {
         sequelize,
